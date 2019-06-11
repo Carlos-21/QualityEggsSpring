1c53dfe6aec91ebf1d84d99f2a6b8f49a7671735
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2019-06-11 00:15:12 -0500

    Avance 2.1

--- src/main/java/com/unmsm/fisi/model/PedidoTrabajador.java
+++ src/main/java/com/unmsm/fisi/model/PedidoTrabajador.java
@@ -3,52 +3,38 @@ package com.unmsm.fisi.model;
 import java.util.Date;
 
 public class PedidoTrabajador {
-	private Integer nIdPedido;
-	private String sTipoDocumento;
-	private String sNumeroDocumento;
+	private String idPersona;
 	private Integer nCantidad;
 	private Boolean vEstado;
 	private Date dFecha;
 	private Date tHora;
+	private Integer nIdPedido;
+	private String sTipoDocumento;
+	private String sNumeroDocumento;
 	
 	public PedidoTrabajador() {
 		super();
 	}
-	
-	public PedidoTrabajador(Integer nIdPedido, String sTipoDocumento, String sNumeroDocumento, Integer nCantidad,
-			Boolean vEstado, Date dFecha, Date tHora) {
+
+	public PedidoTrabajador(String idPersona, Integer nCantidad, Boolean vEstado, Date dFecha, Date tHora,
+			Integer nIdPedido, String sTipoDocumento, String sNumeroDocumento) {
 		super();
-		this.nIdPedido = nIdPedido;
-		this.sTipoDocumento = sTipoDocumento;
-		this.sNumeroDocumento = sNumeroDocumento;
+		this.idPersona = idPersona;
 		this.nCantidad = nCantidad;
 		this.vEstado = vEstado;
 		this.dFecha = dFecha;
 		this.tHora = tHora;
-	}
-
-	public Integer getnIdPedido() {
-		return nIdPedido;
-	}
-
-	public void setnIdPedido(Integer nIdPedido) {
 		this.nIdPedido = nIdPedido;
-	}
-
-	public String getsTipoDocumento() {
-		return sTipoDocumento;
-	}
-
-	public void setsTipoDocumento(String sTipoDocumento) {
 		this.sTipoDocumento = sTipoDocumento;
-	}
-
-	public String getsNumeroDocumento() {
-		return sNumeroDocumento;
-	}
-
-	public void setsNumeroDocumento(String sNumeroDocumento) {
 		this.sNumeroDocumento = sNumeroDocumento;
+	}
+
+	public String getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(String idPersona) {
+		this.idPersona = idPersona;
 	}
 
 	public Integer getnCantidad() {
@@ -81,6 +67,30 @@ public class PedidoTrabajador {
 
 	public void settHora(Date tHora) {
 		this.tHora = tHora;
+	}
+
+	public Integer getnIdPedido() {
+		return nIdPedido;
+	}
+
+	public void setnIdPedido(Integer nIdPedido) {
+		this.nIdPedido = nIdPedido;
+	}
+
+	public String getsTipoDocumento() {
+		return sTipoDocumento;
+	}
+
+	public void setsTipoDocumento(String sTipoDocumento) {
+		this.sTipoDocumento = sTipoDocumento;
+	}
+
+	public String getsNumeroDocumento() {
+		return sNumeroDocumento;
+	}
+
+	public void setsNumeroDocumento(String sNumeroDocumento) {
+		this.sNumeroDocumento = sNumeroDocumento;
 	}
 	
 }
